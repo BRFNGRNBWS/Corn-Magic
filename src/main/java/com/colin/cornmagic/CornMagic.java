edc2d2b802fe0ab9a2b317452e238baea1025312
Unknown <clnlws44@gmail.com>
2019-03-12 21:25:02 -0700

    rough draft due whoops

---
+++ src/main/java/com/colin/cornmagic/CornMagic.java
@@ -0,0 +1,63 @@
+package com.colin.cornmagic;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.item.Item;
+import scala.util.Random;
+
+@Mod(modid = CornMagic.MODID, version = CornMagic.VERSION)
+public class CornMagic
+{
+    public static final String MODID = "cornmagic";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        Item amethyst = new ModItem("amethyst");
+        GameRegistry.registerItem(amethyst, "amethyst");
+
+		Block amethystOre = new ModBlock(Material.rock, "amethystOre", 5.0F, "pickaxe", 1, amethyst);
+		GameRegistry.registerBlock(amethystOre, "amethystOre");
+    }
+
+    private class ModBlock extends Block
+    {
+        private Item toDrop;
+
+        public ModBlock(Material material, String blockName, float hardness, String tool, int level, Item toDrop)
+        {
+            super(material);
+            this.setBlockName(blockName);
+            this.setBlockTextureName(MODID + ":" + blockName);
+            this.setCreativeTab(CreativeTabs.tabBlock);
+            this.setHardness(hardness);
+            this.setHarvestLevel(tool, level);
+            this.toDrop = toDrop;
+        }
+
+        public Item getItemDropped(int i, Random random, int j) {
+            if(toDrop != null)
+            {
+                return toDrop;
+            }
+            else return Item.getItemFromBlock(this);
+        }
+    }
+
+    private class ModItem extends Item
+    {
+        public ModItem(String itemName)
+        {
+            this.setUnlocalizedName(itemName);
+            this.setTextureName(MODID + ":" + itemName);
+            this.setCreativeTab(CreativeTabs.tabMisc);
+        }
+    }
+}
